3b84df38d89ffca44e290cb3011b21115c56267b
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-28 21:14:20 +0000

    lang fix 1

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -8,16 +8,6 @@ export const { staticGET: GET } = createFromSource(source, {
   // https://docs.orama.com/docs/orama-js/supported-languages
   // 不同语言的搜索配置
   localeMap: {
-    // [locale]: Orama options
-    cn: {
-      components: {
-        tokenizer: createTokenizer(),
-      },
-      search: {
-        threshold: 0,
-        tolerance: 0,
-      },
-    },
     en: { language: 'english' },
   },
   buildIndex(page) {
