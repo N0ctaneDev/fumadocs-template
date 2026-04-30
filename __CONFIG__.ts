21c7ad57581321024fd1484558b0bf7867bd1d27
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-30 22:57:02 +0530

    Re-add siteConfig to __CONFIG__.ts

--- __CONFIG__.ts
+++ __CONFIG__.ts
@@ -7,17 +7,17 @@ export const siteMetadata = {
   author: "N0ctaneDev",
 };
 
-// site config
-export const siteConfig = {
-  basePath: `/${githubConfig.repo}`, // set it to '' if you want to deploy to root (https://username.github.io/)
-  name: "Fumadocs Template", // shown on the navbar and used as default title suffix
-};
-
 // github config
 export const githubConfig = {
   repo: "fumadocs-template",
   owner: "n0ctanedev",
   branch: "main",
+};
+
+// site config
+export const siteConfig = {
+  basePath: `/${githubConfig.repo}`, // set it to '' if you want to deploy to root (https://username.github.io/)
+  name: "Fumadocs Template", // shown on the navbar and used as default title suffix
 };
 
 //the folders in /content... u can make separate folders for diff projects and add them here
