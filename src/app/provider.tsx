778c980820bb9905d300abe4b668122b40e55b6b
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-05-01 06:43:56 +0530

    cubv

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,7 +4,7 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import dynamic from "next/dynamic";
 import type { ReactNode } from "react";
 
-import { SearchDialog } from "@/components/search";
+import { DefaultSearchDialog } as SearchDialog from "@/components/search";
 
 export function Provider({ children }: { children: ReactNode }) {
   return (
