eddde6de2faa0a3ffdd368214e14b8af6e4ae394
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-05-02 05:31:02 +0530

    Fix locale and update search API path

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -102,8 +102,7 @@ export default function DefaultSearchDialog(props: SharedProps) {
   const { search, setSearch, query } = useDocsSearch({
     type: "static",
     initOrama,
-    locale: "en",
-    from: `/${siteConfig.basePath}/api/search`,
+    from: `${siteConfig.basePath}/api/search`,
     // tag: effectiveTag,
   });
 
